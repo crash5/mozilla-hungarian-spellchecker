afa44af79643f90adbf02b684b4eb43404c35a03
Imre NAGY <crash5@users.noreply.github.com>
2019-02-02 15:21:24 +0100

    Add manifest.json to support Firefox 65

--- scripts/create_mozilla_dictionary.sh
+++ scripts/create_mozilla_dictionary.sh
@@ -4,10 +4,11 @@ set -e
 
 readonly ISPELL_DICTIONARIES_SOURCE_PATH=$(toAbsolutePath "${1}")
 readonly INSTALL_RDF_TEMPLATE=$(toAbsolutePath "${2}")
-readonly OUTPUT_FILE=$(toAbsolutePath ${3:-"hungarian_dictionary.xpi"})
+readonly MANIFEST_JSON_TEMPLATE=$(toAbsolutePath "${3}")
+readonly OUTPUT_FILE=$(toAbsolutePath ${4:-"hungarian_dictionary.xpi"})
 
-if [[ ${#@} -lt 2 ]]; then
-    echo "Usage: ${0} <ispell dicitonaries path> <template for install rdf> [output extension file]"
+if [[ ${#@} -lt 3 ]]; then
+    echo "Usage: ${0} <ispell dicitonaries path> <template for install rdf> <template for manifest json> [output extension file]"
     exit
 fi
 
@@ -20,6 +21,7 @@ cp "${ISPELL_DICTIONARIES_SOURCE_PATH}"/hu_HU_u8_gen_alias.dic dictionaries/hu.d
 cp "${ISPELL_DICTIONARIES_SOURCE_PATH}"/OLVASSEL dictionaries/README.hu
 cp "${ISPELL_DICTIONARIES_SOURCE_PATH}"/README dictionaries/README.en
 mo "${INSTALL_RDF_TEMPLATE}" > install.rdf
+mo "${MANIFEST_JSON_TEMPLATE}" > manifest.json
 zip -r "${OUTPUT_FILE}" .
 
 cd ..
